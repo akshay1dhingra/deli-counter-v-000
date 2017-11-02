7547da904f7de76479039d383f658ecac08d66ef
Akshay Dhingra <akshay1dhingra@gmail.com>
2017-11-02 17:05:19 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,5 +17,8 @@ def take_a_number(deli, name)
 end
 
 def now_serving(deli)
-
+  if deli.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{deli.first}"
 end
